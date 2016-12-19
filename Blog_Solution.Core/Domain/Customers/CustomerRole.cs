4c6e5fa3832932263fd3b96097cc8bd8727e0516
5ina <elvin0449@qq.com>
2016-12-19 19:45:07 +0800

    新增用户角色实体

---
+++ Blog_Solution.Core/Domain/Customers/CustomerRole.cs
@@ -0,0 +1,47 @@
+﻿using Abp.Domain.Entities;
+using System.ComponentModel.DataAnnotations;
+
+namespace Blog_Solution.Domain.Customers
+{
+    /// <summary>
+    /// 用户角色
+    /// </summary>
+    public class CustomerRole :Entity
+    {
+        /// <summary>
+        /// 是否启用
+        /// </summary>
+        [Required]
+        public bool Enabled { get; set; }
+
+        /// <summary>
+        /// 角色名称
+        /// </summary>
+        [Required, MaxLength(20)]
+        public string RoleName { get; set; }
+
+        /// <summary>
+        /// 排序
+        /// </summary>
+        [Required]
+        public int DisplayOrder { get; set; }
+
+        /// <summary>
+        /// 是否后台用户
+        /// </summary>
+        [Required]
+        public bool IsAdmin { get; set; }
+
+        /// <summary>
+        /// 父节点
+        /// </summary>
+        [Required]
+        public int ParentId { get; set; }
+
+        /// <summary>
+        /// 是否默认注册用户
+        /// </summary>
+        [Required]
+        public bool IsDefault { get; set; }
+    }
+}
