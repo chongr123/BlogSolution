5d47f559b3dcda7365d7f6c6a41a842ca826c44b
5ina <elvin0449@qq.com>
2016-12-20 08:57:41 +0800

    完善用户驱动设计模块

---
+++ Blog_Solution.Core/Domain/Customers/Customer.cs
@@ -0,0 +1,87 @@
+﻿using Abp.Domain.Entities.Auditing;
+using System;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Blog_Solution.Domain.Customers
+{
+    /// <summary>
+    /// 用户实体
+    /// </summary>
+    public class Customer: IHasCreationTime,IHasModificationTime
+    {
+        /// <summary>
+        /// 登录名
+        /// </summary>
+        [Required, MaxLength(50), MinLength(5)]
+        public string LoginName { get; set; }
+
+        /// <summary>
+        /// 密码
+        /// </summary>
+        [Required, MaxLength(60), MinLength(10)]
+        public string Password { get; set; }
+
+        /// <summary>
+        /// 用户角色
+        /// </summary>
+        [Required]
+        public int CustomerRoleId { get; set; }        
+
+        /// <summary>
+        /// 手机号
+        /// </summary>
+        [Required, MaxLength(12), MinLength(11)]
+        public string Mobile { get; set; }
+        /// <summary>
+        /// 邮箱
+        /// </summary>
+        [Required, MaxLength(200), MinLength(8)]
+        public string Email { get; set; }
+        
+        /// <summary>
+        /// 秘钥
+        /// </summary>
+        [Required, MaxLength(20)]
+        public string PasswordSalt { get; set; }
+
+        /// <summary>
+        /// 加密方式
+        /// </summary>
+        [Required]
+        public int PasswordFormatId { get; set; }
+        /// <summary>
+        /// 是否删除
+        /// </summary>
+        [Required]
+        public bool Deleted { get; set; }
+
+
+        /// <summary>
+        /// 最后登录IP
+        /// </summary>
+        [MaxLength(16)]
+        public string LastIpAddress { get; set; }
+        
+        /// <summary>
+        /// 最后修改时间（不需要处理）
+        /// </summary>
+        public DateTime? LastModificationTime { get; set; }
+
+        /// <summary>
+        /// 创建时间（不需要处理）
+        /// </summary>
+        public DateTime CreationTime { get; set; }
+
+
+        /// <summary>
+        /// Gets or sets the password format
+        /// </summary>
+        [NotMapped]
+        public virtual PasswordFormat PasswordFormat
+        {
+            get { return (PasswordFormat)PasswordFormatId; }
+            set { this.PasswordFormatId = (int)value; }
+        }
+    }
+}
---
+++ Blog_Solution.Core/Domain/Customers/PasswordFormat.cs
@@ -0,0 +1,12 @@
+﻿namespace Blog_Solution.Domain.Customers
+{
+    /// <summary>
+    /// 密码加密的格式枚举
+    /// </summary>
+    public enum PasswordFormat:int
+    {
+        Clear = 0,
+        Hashed = 1,
+        Encrypted = 2
+    }
+}
